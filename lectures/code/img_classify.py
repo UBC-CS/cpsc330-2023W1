810dfc8d8b896d85ebd37722a8a2a6b82608a059
Varada Kolhatkar <kvarada@cs.ubc.ca>
2023-09-05 17:58:16 -0700

    fixed imgae classification code

--- lectures/code/img_classify.py
+++ lectures/code/img_classify.py
@@ -8,37 +8,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-# Load the pre-trained VGG16 model
+# Load the VGG16 model pre-trained on ImageNet data
 vgg16 = models.vgg16(pretrained=True)
 vgg16.eval()  # Set the model to evaluation mode
 
-def classify_image(img, topn=4): 
-    # Preprocess the image
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+# Define a function to classify an image
+def classify_image(image_path, class_labels_file):
+    # Load and preprocess the image
+    img = Image.open(image_path)
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    img_tensor = transform(img)
-    img_tensor = img_tensor.unsqueeze(0)  # Add a batch dimension
-    
-    # Make a prediction
+    img = preprocess(img).unsqueeze(0)  # Add a batch dimension
+
+    # Perform inference
     with torch.no_grad():
-        output = vgg16(img_tensor)
-    
-    # Load the class labels (from ImageNet)
-    with open("../data/imagenet_classes.txt") as f:
+        output = vgg16(img)
+
+    # Load the class labels from the text file
+    with open(class_labels_file, 'r') as f:
         class_labels = [line.strip() for line in f.readlines()]
-    
-    # Get the predicted class index
-    predicted_idx = torch.argmax(output).item()
-    _, indices = torch.sort(output, descending=True)
-    probabilities = torch.nn.functional.softmax(output, dim=1)
-    print("Probabilities calculated")
-    
-    d = {'Class': [class_labels[idx] for idx in indices[0][:topn]], 
-        'Probability score': [np.round(probabilities[0, idx].item(),3) for idx in indices[0][:topn]]}
-    print(pd.DataFrame(d))
-    # Print the predicted class label
-    predicted_label = class_labels[predicted_idx]
-    print(f"Predicted class: {predicted_label}")
+
+    # Get the class label with the highest probability
+    _, predicted_class_idx = torch.max(output, 1)
+    predicted_class = class_labels[predicted_class_idx]
+
+    return predicted_class
